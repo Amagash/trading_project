eb371a00f6eaa19d5046784a5cdad437dd0b1e11
Tiffany Souterre <tiffanysouterre@gmail.com>
2018-04-08 01:39:03 +0200

    save model weights for lstm

--- capstone.py
+++ capstone.py
@@ -1,8 +1,10 @@
 import utilities as util
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 
 def run():
@@ -21,38 +23,15 @@ def run():
     data_scaled = scaler.fit_transform(weighted_price)
     # print(data_scaled)
 
-    #plot original data
-    plt.figure(figsize=(10, 10))
-    plt.plot(data_scaled, 'g', label='original dataset')
-    plt.legend(loc='upper left')
-    plt.xlabel('Timestamp')
-    plt.ylabel('Price')
-    # plt.show()
-
+    look_back = 5
     train_set, test_set = util.split_data(data_scaled, train_percentage=0.85)
-
     x_train, y_train = util.create_labels(train_set, look_back=5)
     x_test, y_test = util.create_labels(test_set, look_back=5)
 
     model = util.build_model()
-
-
-    #
-    # history = model.fit(x_train, y_train,
-    #                     batch_size=64,
-    #                     epochs=30,
-    #                     verbose=2,
-    #                     validation_split=0.2)
-
-    # plt.plot(history.history['loss'])
-    # plt.plot(history.history['val_loss'])
-    # plt.title('model loss')
-    # plt.ylabel('loss')
-    # plt.xlabel('epoch')
-    # plt.legend(['train', 'validation'], loc='upper left')
-    # plt.show()
-
-
+    history = util.train_model(model, x_train, y_train)
+    util.plot_training_history(history)
+    model.load_weights('saved_models/weights.best.lstm.hdf5')
 
 
 
