35fa7024ab085ad3f1b6dc8b4c0fd0815adf88ff
Tiffany Souterre <tiffanysouterre@gmail.com>
2018-04-07 22:07:00 +0200

    add split data in main

--- capstone.py
+++ capstone.py
@@ -11,14 +11,14 @@ def run():
     # print(data.tail())
 
     weighted_price = data.Weighted_Price.values.astype('float32')
-    print(weighted_price)
+    # print(weighted_price)
     weighted_price = weighted_price.reshape(len(weighted_price), 1)
-    print(weighted_price)
+    # print(weighted_price)
 
     #scale data
     scaler = MinMaxScaler(feature_range=(0, 1))
     data_scaled = scaler.fit_transform(weighted_price)
-    print(data_scaled)
+    # print(data_scaled)
 
     #plot original data
     plt.figure(figsize=(10, 10))
@@ -26,7 +26,9 @@ def run():
     plt.legend(loc='upper left')
     plt.xlabel('Timestamp')
     plt.ylabel('Price')
-    plt.show()
+    # plt.show()
+
+    utilities.split_data(data_scaled, train_percentage=0.85)
 
 
 if __name__ == "__main__":
