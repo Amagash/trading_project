faf1b4df7448378857210ba9702f0a622abafdeb
Tiffany Souterre <tiffanysouterre@gmail.com>
2018-04-07 20:23:17 +0200

    define main function

--- capstone.py
+++ capstone.py
@@ -1,5 +1,9 @@
 import utilities
 
-currency_data = utilities.get_dataset()
-print(currency_data)
+def run():
+    currency_data = utilities.get_dataset()
+    print(currency_data.head())
 
+
+if __name__ == "__main__":
+    run()
