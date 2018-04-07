1dfe93edbe8737edd93cf8068cb88da0420ae384
Tiffany Souterre <tiffanysouterre@gmail.com>
2018-04-07 21:27:35 +0200

    scale data and plot original data

--- capstone.py
+++ capstone.py
@@ -1,8 +1,32 @@
 import utilities
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+
 
 def run():
-    currency_data = utilities.get_dataset()
-    print(currency_data.head())
+    #load data in dataframe
+    data = utilities.get_dataset()
+    # print(data.head())
+    # print(data.tail())
+
+    weighted_price = data.Weighted_Price.values.astype('float32')
+    print(weighted_price)
+    weighted_price = weighted_price.reshape(len(weighted_price), 1)
+    print(weighted_price)
+
+    #scale data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data_scaled = scaler.fit_transform(weighted_price)
+    print(data_scaled)
+
+    #plot original data
+    plt.figure(figsize=(10, 10))
+    plt.plot(data_scaled, 'g', label='original dataset')
+    plt.legend(loc='upper left')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Price')
+    plt.show()
 
 
 if __name__ == "__main__":
