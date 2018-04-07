bacd14e3259dee9f2bea0e44739d15020ec048bf
Tiffany Souterre <tiffanysouterre@gmail.com>
2018-04-08 01:41:45 +0200

    add train model to utilities

--- utilities.py
+++ utilities.py
@@ -3,6 +3,8 @@ import numpy as np
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
+import matplotlib.pyplot as plt
+from keras.callbacks import ModelCheckpoint
 
 def get_dataset():
     folder_path = "data/bitstampUSD_1-min_data_2012-01-01_to_2018-03-27.csv"
@@ -29,6 +31,25 @@ def create_labels(data, look_back):
 
     return data_x, data_y
 
+def plot_original_data(data):
+    #plot original data
+    plt.figure(figsize=(10, 10))
+    plt.plot(data, 'g', label='original dataset')
+    plt.legend(loc='upper left')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Price')
+    plt.show()
+
+def plot_training_history(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+
 def build_model():
     model = Sequential()
 
@@ -37,8 +58,7 @@ def build_model():
                     return_sequences=True))
     model.add(Dropout(0.35))
 
-    model.add(LSTM(100,
-                    return_sequences=False))
+    model.add(LSTM(100, return_sequences=False))
     model.add(Dropout(0.35))
 
     model.add(Dense(units=1))
@@ -47,3 +67,19 @@ def build_model():
     model.compile(loss='mse', optimizer='rmsprop')
     model.summary()
     return model
+
+def train_model(model, x_train, y_train):
+    checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.lstm.hdf5',
+                                   verbose=1,
+                                   save_best_only=True)
+
+    history = model.fit(x_train, y_train, batch_size=64,
+                                epochs=5,
+                                verbose=1,
+                                validation_split=0.2,
+                                callbacks=[checkpointer])
+
+    return history
+
+
+
